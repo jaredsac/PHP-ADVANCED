552519181cc394d8d8d1eec7c46d081b8c3addfe
Jared Sackey <jared.sackey@student.rocva.nl>
2020-06-15 15:24:48 +0200

    klaar

---
+++ 2-Loops/taak01/while-loop.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+$leeftijd = 0;
+ 
+while($leeftijd <= 17) {
+  echo "ik ben $leeftijd jaar, dus ik mag nog niet stemmen <br>";
+  $leeftijd++;
+} 
+echo "ik ben 18 jaar old, dus ik heb stemrecht!";
+?>  
+
+</body>
+</html>
