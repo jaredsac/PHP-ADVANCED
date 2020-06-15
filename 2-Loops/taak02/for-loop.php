c38af885e13e9847b3ae2c24206b9dccad040ab3
Jared Sackey <jared.sackey@student.rocva.nl>
2020-06-15 15:30:46 +0200

    klaar

---
+++ 2-Loops/taak02/for-loop.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+for ($leeftijd = 0; $leeftijd <= 17; $leeftijd++) {
+  echo "ik ben $leeftijd jaar, dus ik mag nog niet stemmen <br>";
+}
+echo "Ik ben 18 jaar oud dus ik heb stemrecht!";
+?>  
+
+</body>
+</html>
