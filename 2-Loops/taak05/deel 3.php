9839ce3cb355c5998ed9a69f93e92168fc993d96
Jared Sackey <jared.sackey@student.rocva.nl>
2020-06-24 11:39:12 +0200

    bijna

---
+++ 2-Loops/taak05/deel 3.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+    $klassen = array(
+        $student  = array(
+        "voornaam" => "Jan",
+        "achternaam" => "Janssen",
+        "klas" => "9A",
+        "Leeftijd" => 17,
+        "Woonplaats" => "Amstelveen"
+        );
+   
+foreach($klassen as $student => $value) {
+    echo "De lengte van $key is $value <br>";
+  }
+  
+
+
+
+
+
+?>
+
+
+</body>
+</html>
